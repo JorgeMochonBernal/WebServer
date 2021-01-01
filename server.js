c33cebb00138f8d8fae061e89083c71918973eaf
JorgeMochonBernal <jorgemochonb@gmail.com>
2021-01-01 20:26:50 +0100

    Helpser y hbs finalizados

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 
   const express = require('express');
   const app = express();
+  const hbs = require('hbs');
 
   app.use(express.static(__dirname + '/public'));
 
-  app.listen(3000, () => {
-      console.log('Escuchando peticiones');
-  });
+  hbs.registerPartials(__dirname + '/views/parciales');
 
   app.set('view engine', 'hbs');
+
+  hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+  });
+
+  hbs.registerHelper('capitalizar', (texto) => {
+    let palabras = texto.split('');
+
+    palabras.forEach((palabra, idx) => {
+      palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLocaleLowerCase();
+    });
+
+    return palabras.join('');
+    
+  });
 
   app.get('/', (req, resp) => {
     resp.render('home', {
       nombre: 'Jorge',
-      anio: new Date().getFullYear()
     });
+  });
+
+  app.get('/about',(req,resp) => {
+    resp.render('about', {
+    });
+  });
+
+  app.listen(3000, () => {
+      console.log('Escuchando peticiones');
   });
